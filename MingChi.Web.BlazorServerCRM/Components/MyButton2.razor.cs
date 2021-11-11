e72d0eaed58049c788c5f52c16da476d90abc464
wugelis <wergelis@msn.com>
2021-11-11 16:11:40 +0800

    將 MyButton2 修改為 Code-behind 形式

---
+++ MingChi.Web.BlazorServerCRM/Components/MyButton2.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MingChi.Web.BlazorServerCRM.Components
+{
+    public partial class MyButton2
+    {
+        [Parameter]
+        public string ButtonText { get; set; }
+    }
+}
